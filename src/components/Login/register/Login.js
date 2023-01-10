89bd96833f7715b3bc5e5bba4e46f274d33e3cfa
Gloria-Abedi-Gyemfa <gloria.gyemfa@amalitech.com>
2023-01-10 15:32:32 +0000

    created a login page

--- src/components/Login/register/Login.js
+++ src/components/Login/register/Login.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import "./Login.css";
+import login from "../../../images/login.jpg"
+import { Link } from 'react-router-dom';
+import {IoMdArrowRoundBack} from "react-icons/io"
 
 const Login = () => {
   return (
     <div>
+      
+    <div className='login'>
+      <Link to='/startup'><IoMdArrowRoundBack/></Link>
+    
+      <img src={login} className='login-image'/>
       <form>
         <h2>LOGIN</h2>
+        <input placeholder='E-mail ' className='email form-control'/>
+        <input placeholder='Password' className='password form-control'/>
+        <button>Login</button>
+        {/* <Link to='/signup'>
+        <p>signup</p>
+        </Link> */}
       </form>
+    </div>
     </div>
   )
 }
