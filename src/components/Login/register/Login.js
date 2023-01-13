e42ad982d852b686c2fb510b21381b469eb4a38a
Gloria-Abedi-Gyemfa <gloria.gyemfa@amalitech.com>
2023-01-13 14:07:28 +0000

    worked on the input form validity

--- src/components/Login/register/Login.js
+++ src/components/Login/register/Login.js
@@ -4,7 +4,28 @@ import login from "../../../images/login.jpg"
 import { Link } from 'react-router-dom';
 import {IoMdArrowRoundBack} from "react-icons/io"
 
-const Login = () => {
+class Login extends React.Component {
+  constructor (props)
+  {
+    super(props);
+    this.state = {
+      email: '',
+      password: ''
+    }
+    // this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange = (e) => {
+    this.setState({
+        [e.target.name]: e.target.value
+    });
+}
+
+handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+}
+render(){
   return (
     <div>
       
@@ -12,11 +33,15 @@ const Login = () => {
       <Link to='/startup'><IoMdArrowRoundBack/></Link>
     
       <img src={login} className='login-image'/>
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <h2>LOGIN</h2>
-        <input placeholder='E-mail ' className='email form-control'/>
-        <input placeholder='Password' className='password form-control'/>
-        <button>Login</button>
+        <input type="email" placeholder='E-mail ' className='email form-control'
+        onChange={this.handleChange}
+        />
+        <input type="password" placeholder='Password' className='password form-control'
+        onChange={this.handleChange}
+        />
+        <button type='submit'>Login</button>
         {/* <Link to='/signup'>
         <p>signup</p>
         </Link> */}
@@ -24,6 +49,7 @@ const Login = () => {
     </div>
     </div>
   )
+}
 }
 
 export default Login
