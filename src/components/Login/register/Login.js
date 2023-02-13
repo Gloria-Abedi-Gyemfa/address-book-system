ca999a77d588a4240ccc24595b88bb4d885baf99
Gloria-Abedi-Gyemfa <gloria.gyemfa@amalitech.com>
2023-02-13 13:33:26 +0000

    validate user input

--- src/components/Login/register/Login.js
+++ src/components/Login/register/Login.js
@@ -6,37 +6,45 @@ import { IoMdArrowRoundBack } from "react-icons/io";
 
 import axios from "axios";
 import api from "../../../services/Api";
-import Cookies from "js-cookie";
-
-
+// import Cookies from "js-cookie";
 
 const Login = () => {
- 
-  const [getEmail, setGetEmail] = useState("")
-  const [getPassword, setGetPassword] = useState("")
-  const [accessToken, setAccessToken]= useState()
+  // const [getEmail, setGetEmail] = useState("")
+  // const [getPassword, setGetPassword] = useState("")
+  const [accessToken, setAccessToken] = useState();
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+  });
+  const handleEmail = (e) => {
+    setValues({ ...values, email: e.target.value });
+  };
+  const handlePassword = (e) => {
+    setValues({...values, password: e.target.value});
+  };
+  const [submitted, setSubmitted] = useState(false)
+
+  const [valid, setValid] = useState(false)
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const data = {
-      email: getEmail,
-      password: getPassword
-    }
-    
-  
- 
+      email: handleEmail,
+      password: handlePassword
+    };
     api().post("/auth/login", data)
-    .then((res)=>{
-      console.log(res)
-    })
-      
+      .then((res) => {
+        console.log(res);
+      });
+    if (values.email && values.password) {
+      setValid(true)
+    }
+    setSubmitted(true)
+    
   };
-  let setting = browser.cookies.set(
-    {}               // object
-  )
-
-  
-
+  // let setting = browser.cookies.set(
+  //   {}               // object
+  // )
 
   return (
     <div>
@@ -45,24 +53,30 @@ const Login = () => {
           <IoMdArrowRoundBack />
         </Link>
 
-        <img src={login} className="login-image img-fluid" />
+        <img src={login} className="login-image img-fluid" alt="login-vector" />
 
         <form onSubmit={handleSubmit}>
+
+          {submitted && valid? <div className="success-message">Success!</div> : null}
           <h2>LOGIN</h2>
-          <input 
+          <input
+            value={values.email}
             type="email"
             placeholder="E-mail... "
             className="email form-control"
-            onChange={(e) => setGetEmail(e.target.value)}
-            required
+            onChange={handleEmail}
+            
           />
+          {submitted && !values.email ? <span className="validation">Please type your email</span> : null}
           <input
+            value={values.password}
             type="password"
             placeholder="Password..."
             className="password form-control"
-            onChange={(e)=> setGetPassword(e.target.value)}
-            required
+            onChange={handlePassword}
+            
           />
+          {submitted && !values.password ? <span className="validation">Enter your Password</span> : null}
           <button type="submit">Login</button>
 
           <div className="alt">
