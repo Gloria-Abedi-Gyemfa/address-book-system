f0372337580758f8a9676c27a12c3bb1fcbfede5
Gloria-Abedi-Gyemfa <gloria.gyemfa@amalitech.com>
2023-05-17 11:38:41 +0000

    changed the alert html tags

--- src/components/alert/Alert.js
+++ src/components/alert/Alert.js
@@ -4,11 +4,11 @@ import style from './alert.module.css'
 
 const Alert = ({ message, type }) => {
   return (
-    <section className={style.alert}>
-      <h2 className={type === 'error' ? style.unsuccessful : style.successful}>
+    <div className={style.alert}>
+      <p className={type === 'error' ? style.unsuccessful : style.successful}>
         {message}
-      </h2>
-    </section>
+      </p>
+    </div>
   )
 }
 
