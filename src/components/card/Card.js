491150130dcc800fa9291ecc79a904b1357bf6aa
Gloria-Abedi-Gyemfa <117346782+Gloria-Abedi-Gyemfa@users.noreply.github.com>
2023-05-19 15:01:58 +0000

    worked on the card component (#3)

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,11 +1,10 @@
 import React from 'react'
 import cardStyles from './card.module.css'
-import authStyles from '../services/authLayout.module.css'
 
 const Card = ({title, children}) => {
   return (
     <>
-      <div className={authStyles.Card}>
+      <div className={cardStyles.card}>
         <h1 className={cardStyles.heading}>{title}</h1>
         <div>{children}</div>
       </div>
