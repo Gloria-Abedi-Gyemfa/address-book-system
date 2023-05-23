73c2a83c47582e9eab029b711da30ad821eeedc8
Gloria-Abedi-Gyemfa <gloria.gyemfa@amalitech.com>
2023-05-23 09:36:48 +0000

    fixed conflict

--- src/components/forms/signup/SignupForm.js
+++ src/components/forms/signup/SignupForm.js
@@ -9,22 +9,16 @@ import formStyles from '../forms.module.css'
 const SignupForm = ({ handleSubmit, loader }) => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
-<<<<<<< Updated upstream
-  const [name, setName] = useState('')
-  const [loader, setLoader] = useState(false)
-=======
   const [firstName, setFirstName] = useState('')
   const [lastName, setLastName] = useState('')
->>>>>>> Stashed changes
 
   return (
     <>
       <form onSubmit={handleSubmit}>        
           <EmailInput email={email} setEmail={setEmail} label='Email'/>
           <PasswordInput password={password} setPassword={setPassword} label='Password' />
-<<<<<<< Updated upstream
-          <TextInput label='First Name'  setName={setName} name={name} />
-          <TextInput label='Last Name' setName={setName} name={name}/>
+          <TextInput label='First Name'  setName={setName} name={firstName} />
+          <TextInput label='Last Name' setName={setName} name={lastName}/>
           <Button variant = 'primary' size='large' text='submit' loader={loader}/>
           <p className={formStyles.alt}>or</p>
           <Link to="/" >
