d43b1c87d9bae86e709c2a34d2ca2a869474c523
Gloria-Abedi-Gyemfa <117346782+Gloria-Abedi-Gyemfa@users.noreply.github.com>
2023-05-23 10:39:11 +0000

    Feature (#10)
    * worked on the card component
    
    * declared a loader state
    
    * Input component (#7)
    
    * Dev (#2)
    
    * worked on text component and created a base url
    
    * renamed an axios instance
    
    * created a card component and changed the styles for the login/signup page
    
    * worked on the card component
    
    * completed 3 tickets
    
    * fixed errors on review
    
    * fixed errors on review
    
    * worked on validation
    
    * worked on the style module
    
    * woked on the css module for the signup/login page
    
    * worked on the loader component
    
    * changed the alert html tags
    
    * removed hook-form from package.json
    
    * resolved merge errors
    
    * Button component (#8)
    
    * Dev (#2)
    
    * worked on text component and created a base url
    
    * renamed an axios instance
    
    * created a card component and changed the styles for the login/signup page
    
    * worked on the card component
    
    * completed 3 tickets
    
    * fixed errors on review
    
    * fixed errors on review
    
    * worked on validation
    
    * worked on the style module
    
    * woked on the css module for the signup/login page
    
    * worked on the loader component
    
    * changed the alert html tags
    
    * removed hook-form from package.json
    
    * resolved merge errors
    
    * worked on the button component in the various form cards
    
    * worked on error messages (#9)
    
    * worked on error messages
    
    * fixed conflict

--- src/components/forms/signup/SignupForm.js
+++ src/components/forms/signup/SignupForm.js
@@ -6,22 +6,24 @@ import PasswordInput from '../input/PasswordInput'
 import TextInput from '../input/TextInput'
 import formStyles from '../forms.module.css'
 
-const SignupForm = ({ handleSubmit }) => {
+const SignupForm = ({ handleSubmit, loader }) => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
-  const [name, setName] = useState('')
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
 
   return (
     <>
       <form onSubmit={handleSubmit}>        
           <EmailInput email={email} setEmail={setEmail} label='Email'/>
           <PasswordInput password={password} setPassword={setPassword} label='Password' />
-          <TextInput label='First Name'  setName={setName} name={name} />
-          <TextInput label='Last Name' setName={setName} name={name}/>
-          <Button variant = 'primary' size='large' text='submit' loader={loader} />
-          <div className={formStyles.alt}>
-            <Link to="/">Login</Link>
-          </div>
+          <TextInput label='First Name'  setName={setName} name={firstName} />
+          <TextInput label='Last Name' setName={setName} name={lastName}/>
+          <Button variant = 'primary' size='large' text='submit' loader={loader}/>
+          <p className={formStyles.alt}>or</p>
+          <Link to="/" >
+            <Button variant='secondary' size='large' text='Login' />
+          </Link>         
       </form>
     </>
   )
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
 import AuthLayout from '../components/services/authLayout'
 import LoginCard from '../components/forms/login/LoginCard'
+import Alert from '../components/alert/Alert'
 
 const LoginPage = () => {
+  const [alert, setAlert] = useState({ show: false, message: '', type: '' })
+  const handleAlert = (value)=>{
+    setAlert(value)
+  }
   return (
+    <>
+    {alert.show && <Alert type={alert.type} message={alert.message} />}
     <AuthLayout>
-        <LoginCard/>
+        <LoginCard onAlert={handleAlert}/>
      </AuthLayout>
+    </>
   )
 }
 
